06e4158d811341e69a649acd8ba591f097638851
Salomon700 <ntwalisolomon9@gmail.com>
2024-06-07 19:09:58 +0200

    fixed betty coding style issue

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,10 +1,12 @@
 #include "binary_trees.h"
 
-/*
+/**
  * binary_tree_insert_left - add node to left of parent
  * if it already exists move down 1 level and add node first
+ *
  * @parent: parent of specific node
  * @value: value of node
+ *
  * Return: if fails NULL else new node
  */
 
@@ -20,7 +22,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new_node = binary_tree_node(parent, value);
 	if (new_node == NULL)
 	{
-		return(NULL);
+		return (NULL);
 	}
 	if (parent->left != NULL)
 	{
